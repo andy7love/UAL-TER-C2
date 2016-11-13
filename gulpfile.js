8ebe6ab590214f677801a137aa169effade9dead
Andres Amor <andy.7love@gmail.com>
2016-11-13 01:46:21 -0300

    Fixed typings, intellisense and require for node modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var clean = require('gulp-clean');
+var tsProject = ts.createProject('./tsconfig.json');
  
 gulp.task('clean', function () {
     return gulp.src('build', {read: false})
@@ -8,10 +9,8 @@ gulp.task('clean', function () {
 });
  
 gulp.task('default', ['clean'], function () {
-    return gulp.src('src/**/*.ts')
-        .pipe(ts({
-            noImplicitAny: true,
-            module: 'commonjs'
-        }))
-        .pipe(gulp.dest('build'));
+    var tsResult = tsProject.src()
+        .pipe(tsProject());
+
+    return tsResult.js.pipe(gulp.dest('build'));
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { System } from './system/test';
+let s = require('socket.io');
+let d = require('johnny-five');
 let myClass = new System.MyTest();
 myClass.log();
+console.log(s);
