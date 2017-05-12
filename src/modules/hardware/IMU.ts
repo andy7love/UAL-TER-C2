09498290e2d5231a912959964a6b558dfd017102
Andres Amor <andy.7love@gmail.com>
2017-05-11 21:41:27 -0300

    calibrate imu

--- src/modules/hardware/IMU.ts
+++ src/modules/hardware/IMU.ts
@@ -74,7 +74,7 @@ export class IMU implements DroneModule {
 			console.log("  pitch        : ", this.euler.pitch);
 			console.log("---------------------");
 			
-			let q = new CANNON.Quaternion(this.quarternion.x,this.quarternion.y,this.quarternion.z,this.quarternion.w);
+			let q = new CANNON.Quaternion(this.quarternion.x,this.quarternion.z,this.quarternion.y,this.quarternion.w);
 			/*
 			let rot = new CANNON.Quaternion();
 			rot.setFromAxisAngle(new CANNON.Vec3(0,1,0), Utils.toRadians(-90));
