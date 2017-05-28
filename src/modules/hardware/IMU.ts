96821ca382e7a74bdd78656829e37c5b06ec5ac2
Andres Amor <andy.7love@gmail.com>
2017-05-28 17:34:15 -0300

    imu calibraiton

--- src/modules/hardware/IMU.ts
+++ src/modules/hardware/IMU.ts
@@ -78,7 +78,7 @@ export class IMU implements DroneModule {
 			rot.setFromAxisAngle(new CANNON.Vec3(0,1,0), Utils.toRadians(180));
 			q = q.mult(rot);
 
-			rot.setFromAxisAngle(new CANNON.Vec3(1,0,0), Utils.toRadians(90));
+			rot.setFromAxisAngle(new CANNON.Vec3(0,0,1), Utils.toRadians(90));
 			q = q.mult(rot);
 
 			state.current.orientation.setValue(q);
